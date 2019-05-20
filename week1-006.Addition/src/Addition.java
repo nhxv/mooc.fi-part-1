c5e1e9d7a8199d47b0591d28f15f7adcaaa39975
Xuân Vinh <42742785+nhxv@users.noreply.github.com>
2019-05-20 15:24:32 -0700

    Add files via upload

---
+++ week1-006.Addition/src/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+    }
+}
