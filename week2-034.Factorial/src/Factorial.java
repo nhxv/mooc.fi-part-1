2efa03f40930c862d2f141a55cc73853ac4ba4a0
Xuân Vinh <42742785+nhxv@users.noreply.github.com>
2019-05-25 01:27:13 -0700

    Add files via upload

---
+++ week2-034.Factorial/src/Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        int i = 1;
+        int result = 1;
+
+        while (i <= number) {
+            result *= i;
+            i++;
+        }
+        System.out.println("Factorial is " + result);
+    }
+}
+
